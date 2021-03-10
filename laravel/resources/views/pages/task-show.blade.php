c5141ee2bebd64a24965cfbdb1a62cad768e1dda
Andrea <grassiandrea95@gmail.com>
2021-03-10 17:42:13 +0100

    modifica in task create

--- laravel/resources/views/pages/task-show.blade.php
+++ laravel/resources/views/pages/task-show.blade.php
@@ -7,15 +7,18 @@
         lastname: {{$task -> employee -> lastname}}
     </h1>
 
-    <h1>Typologies</h1>
-    <ul>
-        @foreach ($task -> typologies as $typology)
-            <li>
-                Name: {{$typology-> name}} <br>
-                Description: {{$typology-> description}}
-            </li>
-        @endforeach
-    </ul>
+    @if (count($task -> typologies)>0)
+        <h1>Typologies</h1>
+        <ul>
+            @foreach ($task -> typologies as $typology)
+                <li>
+                    Name: {{$typology-> name}} <br>
+                    Description: {{$typology-> description}}
+                </li>
+            @endforeach
+        </ul>
+    @endif
+    
 
     <h1>Torna a <a href="{{route('tasks.index')}}">Task-index</a></h1>
     
